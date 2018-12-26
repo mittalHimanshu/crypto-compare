45810c5223260ad7430d499e68fbf56990140237
Himanshu Mittal <mittalhimanshu121@gmail.com>
2018-12-26 18:55:38 +0530

    v1.0.0 ready for deployment

--- server.js
+++ server.js
@@ -1,11 +1,20 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const data = require('./routes/api/data')
+const path = require('path')
 
 const app = express()
 
 app.use(bodyParser.json())
 app.use('/api/data', data)
+
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    })
+}
 
 const port = process.env.PORT || 5000
 
